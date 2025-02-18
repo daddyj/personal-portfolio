c97850724b6da30e50875eb886bca4f752d18fa6
Acun Guersoy <acun.guersoy@googlemail.com>
2025-02-18 22:29:42 +0100

    feat: add photo with effect

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,19 +3,24 @@ import { YStack, XStack, H1, H2 } from "tamagui"
 export const About = () => {
   return (
     <YStack width="100vw" height="100vh">
-      <YStack p={24} px={160} height="100vh" justify="space-between">
+      <YStack p={80} px={160} pr={0} pb={0} height="100vh" gap={80}>
         <XStack width="70vw">
           <H1 fontWeight="bold">Software Entwickler / <br /> Team Lead</H1>
         </XStack>
-        {/* <XStack>
-              <XStack style={{
-                borderRadius: 24, overflow: 'hidden'
-              }} width={aboutMePicture.width / 1.5} height={aboutMePicture.height / 1.5} borderColor="$backgroundHover">
-                <Image src='/about-me-2.png' width={aboutMePicture.width / 1.5} height={aboutMePicture.height / 1.5} alt="Picture of me with my dog" />
-              </XStack>
-            </XStack> */}
-        <XStack flex={1} width="50vw" items="flex-end" self="flex-end">
-          <H2 size="$8" text="justify">
+        <XStack justify="flex-end" px={80}>
+          <XStack style={{
+            overflow: 'hidden',
+            backgroundImage: `url("/about-me.png")`,
+            backgroundAttachment: 'fixed',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: '806px 605px',
+            backgroundPositionY: '25%'
+
+          }} width="70vw" height="42vh" />
+        </XStack>
+        <XStack flex={1} width="60vw" items="flex-end" self="flex-end">
+          <H2 size="$8" text="justify" background="#000000" p={24} px={40}>
             Hi, ich bin Acun. Ich unterstütze Entwicklungsteams sowohl aktiv bei der Entwicklung im Web und App Bereich als auch organisatorisch als Team Lead.</H2>
         </XStack>
       </YStack>
