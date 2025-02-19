a7f1fd8510007e5605f54427f9bf13e1e5645032
Acun Guersoy <acun.guersoy@googlemail.com>
2025-02-19 11:47:36 +0100

    feat: implement about section with tailwindcss

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,29 +1,19 @@
-import { YStack, XStack, H1, H2 } from "tamagui"
-
 export const About = () => {
   return (
-    <YStack width="100vw" height="100vh">
-      <YStack p={80} px={160} pr={0} pb={0} height="100vh" gap={80}>
-        <XStack width="70vw">
-          <H1 fontWeight="bold">Software Entwickler / <br /> Team Lead</H1>
-        </XStack>
-        <XStack justify="flex-end" px={80}>
-          <XStack style={{
-            overflow: 'hidden',
-            backgroundImage: `url("/about-me.png")`,
-            backgroundAttachment: 'fixed',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: '806px 605px',
-            backgroundPositionY: '25%'
-
-          }} width="70vw" height="42vh" />
-        </XStack>
-        <XStack flex={1} width="60vw" items="flex-end" self="flex-end">
-          <H2 size="$8" text="justify" background="#000000" p={24} px={40}>
-            Hi, ich bin Acun. Ich unterstütze Entwicklungsteams sowohl aktiv bei der Entwicklung im Web und App Bereich als auch organisatorisch als Team Lead.</H2>
-        </XStack>
-      </YStack>
-    </YStack>
+    <div className="w-screen h-screen bg-[tomato] flex flex-col">
+      <div className="flex flex-col p-[80px] px-[160px] pr-0 pb-0 h-screen gap-[80px]">
+        <div className="w-[70vw]">
+          <h1 className="bold text-6xl">Software Entwickler / <br /> Teamlead</h1>
+        </div>
+        <div className="flex justify-end px-[80px]">
+          <div className="overflow-hidden bg-[url(/about-me.png)] bg-fixed bg-no-repeat bg-top w-[70vw] h-[30vh]" />
+        </div>
+        <div className="flex flex-1 w-[60vw] items-end self-end">
+          <h2 className="text-3xl text-justify bg-black p-16 px-[40px]">
+            Hi, ich bin Acun. Ich unterstütze Entwicklungsteams sowohl aktiv bei der Entwicklung im Web und App Bereich als auch organisatorisch als Team Lead.
+          </h2>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { NextTamaguiProvider } from "@/components/TamaguiNextProvider/TamaguiNex
 import { YStack } from "tamagui";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
-import { Hero, HeroTailwind } from "./components/Hero";
+import { Hero } from "./components/Hero";
 import { Skills } from "./components/Skills";
 import { Testimonials } from "./components/Testimonials";
 import { montserrat } from "../../tamagui.config";
@@ -14,6 +14,7 @@ export default function Home() {
   return (
     <div className="" style={montserrat.style}>
       <Hero />
+      <About />
       <div className="w-screen h-screen"></div>
     </div>
   );
