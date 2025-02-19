8c7a8a7e52862c569eda778b61f655b10e71f5ee
Acun Guersoy <acun.guersoy@googlemail.com>
2025-02-19 12:17:44 +0100

    refactor: remove tamagui dependencies and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import { withTamagui } from '@tamagui/next-plugin'
-
-const tamaguiPlugin = withTamagui({
-  config: './tamagui.config.ts',
-  components: ['tamagui'],
-  appDir: true,
-})
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ...tamaguiPlugin
 };
 
 export default nextConfig;
