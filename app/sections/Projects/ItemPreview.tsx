c37ba1aff3987bbbc1bb480dfb83e8f4097c4b6d
Acun Guersoy <acun.guersoy@googlemail.com>
2025-02-23 16:06:15 +0100

    feat: continue finishing projects section

--- app/sections/Projects/ItemPreview.tsx
+++ app/sections/Projects/ItemPreview.tsx
@@ -1,20 +1,22 @@
 import { GridItem } from "@/app/components/Grid";
-import { Fragment, JSX } from "react";
+import Image from 'next/image';
+import { Fragment } from "react";
+
+interface ItemPreviewProps {
+  role: string;
+  techStack: React.ReactNode[];
+  images: string[];
+  description: React.ReactNode
+}
 
 export const ItemPreview = ({
   role,
   techStack,
   images,
   description
-}: {
-  role: string;
-  techStack: React.ReactNode[];
-  images: JSX.Element[];
-  description: React.ReactNode
-}) => {
+}: ItemPreviewProps) => {
   return (
     <>
-
       <GridItem className='col-span-10 sm:col-span-3 flex flex-col gap-8 animate-fade'>
         <div className='text-md sm:text-2xl'>
           <p className='font-bold'>Rolle in dem Projekt:</p>
@@ -32,12 +34,16 @@ export const ItemPreview = ({
           </div>
         </div>
       </GridItem>
-      <GridItem className='col-span-10 sm:col-span-4 flex justify-start sm:justify-end items-start animate-fade'>
-        {images.map((image, index) => (
-          <div key={`${role}_image_${index}`} className="flex rounded-sm overflow-hidden">
-            {image}
-          </div>
-        ))}
+      <GridItem className='col-span-10 sm:col-span-4 flex justify-start sm:justify-end items-start animate-fade relative'>
+        {images.map((image, index) => {
+          const offset = index * 16
+          const rotate = ((index + 1) * 8) - 16
+          return (
+            <div key={`${role}_image_${index}`} className={`self-start py-16 sm:py-0 border-black border-1 self-end right-0 top-0 rounded-sm overflow-hidden shadow-sm z-0 hover:z-10`} style={{ right: offset, rotate: `${rotate}deg`, position: index === 0 ? 'relative' : 'absolute' }}>
+              <Image src={image} alt={`Vorschaubild ${index}`} width={400} height={400} />
+            </div>
+          )
+        })}
       </GridItem>
       <GridItem className='col-span-3' />
       <GridItem className='col-span-10 sm:col-span-7 flex items-end animate-fade' >
--- app/sections/Projects/Projects.tsx
+++ app/sections/Projects/Projects.tsx
@@ -1,48 +1,43 @@
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
 import { Grid, GridItem } from '@/app/components/Grid'
 import { useNavigationContext } from '@/app/lib/useNavigationContext'
 import { useViewportIntersect } from '@/app/lib/useViewportIntersect'
+import { useEffect, useRef, useState } from 'react'
 import { ItemPreview } from './ItemPreview'
 import { NavItem } from './NavItem'
 
 const projects = ['myla', 'theanswer-hp', 'myla-portal', 'own-hp'] as const
-type ProjectListItem = typeof projects[number]
+export type ProjectListItem = typeof projects[number]
 
 const projectDetails = {
   myla: {
     navLabel: 'Myla',
     role: 'Fullstack Entwicklung App und Bereitstellung der Cloud Infrastruktur.',
     techStack: ['React-Native', 'Tamagui UI', 'Firebase', 'Google-Cloud'],
-    images: [
-      <div key="myla-1" className="flex rounded-sm overflow-hidden">
-        <Image src="/myla-1.png" width={330 / 2.5} height={717 / 2.5} alt="Erstes Bild zu Myla App" />
-        <Image src="/myla-2.png" width={330 / 2.5} height={717 / 2.5} alt="Erstes Bild zu Myla App" />
-      </div>
-    ],
+    images: ["/myla-3.png", "/myla-2.png", "/myla-1.png"],
     description: <>Die App Myla hat als Ziel eine individuelle Lernbegleitung für jedes Kind anzubieten. Es nutzt künstliche Intelligenz in einer geführten und interaktiven Erfahrung für die Kinder, so dass Sie spielerisch das selbstständige Lernen erhalten. <br />
-      Die App habe ich vollständig entwickelt und im iOS AppStore für iPads und iPhones veröffentlicht. Android ist auch in Planung. Mehr Informationen findest Du hier: [TODO URL einfügen zu THE ANSWER Homepage]</>,
+      Die App habe ich vollständig entwickelt und im iOS AppStore für iPads und iPhones veröffentlicht. Android ist auch in Planung. Mehr Informationen findest Du <a href="https://the-answer.org" target="_blank">hier</a>:</>
   },
   'theanswer-hp': {
     navLabel: 'THE ANSWER GmbH Homepage',
     role: 'Fullstack Entwicklung Homepage und UI Design',
     techStack: ['React', 'NextJS', 'Material UI'],
-    images: [],
-    description: 'TBD'
+    images: ["/the-answer-3.png", "/the-answer-2.png", "/the-answer-1.png"],
+    description: <>Im Rahmen meiner Firmengründung mit zwei Freunden, um die App Myla auch offiziell zu vertreiben, habe ich die Firmenseite entwickelt. Ich habe mich bewusst dazu entschieden es ohne Baukasten oder CRM zu machen um hier hauptsächlich ein Referenzprojekt als Grundlage zur Diskussion für Kombinationen von Technologien zu haben. <br />
+      Ich finde es immer noch spannend und offen zu sagen, welche Kombination &quot;die Beste&quot; ist.</>
   },
   'myla-portal': {
     navLabel: 'Myla Schulportal',
     role: 'Fullstack Entwicklung Portal und UI Design',
     techStack: ['React', 'NextJS', 'Material UI', 'Firebase'],
-    images: [],
-    description: 'TBD'
+    images: ["/myla-portal-3.png", "/myla-portal-2.png", "/myla-portal-1.png"],
+    description: <>Die App Myla wird sowohl auf dem Privatmarkt vertrieben, als auch für Schulen auf geschäftlicher Ebene über eine jährliche Schullizenz pro Gerät. Hierfür habe ich eine minimale Form von Portal entwickelt um zum Einen den time-to-market nicht zu verzögern und zum Anderen dem Benutzer nur die Informationsmenge und Funktionsvielfalt mitzugeben die zu jeweiligem Entwickklungsstand am hilfreichsten ist.</>
   },
   'own-hp': {
     navLabel: 'Acun Gürsoy Homepage',
     role: 'Fullstack Entwicklung Homepage und UI Design',
     techStack: ['React', 'NextJS', 'Tailwind CSS', 'Canvas'],
-    images: [],
-    description: 'TBD'
+    images: ["/own-hp-3.png", "/own-hp-2.png", "/own-hp-1.png"],
+    description: <>Für eine weitere technologische Fallstudie und um auch eine produktive, technische &quot;Spielwiese&quot; zu haben, habe ich auch meine eigene Homepage entwickelt. Hier waren meine Ziele möglichst zeiteffizient eine flexible und wartbare Lösung zu schaffen.</>
   }
 }
 
