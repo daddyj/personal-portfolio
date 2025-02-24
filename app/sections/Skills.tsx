fb55ea969882fe10d8ed72d4c87e36f00add7f90
Acun Guersoy <acun.guersoy@googlemail.com>
2025-02-24 16:56:16 +0100

    feat: add cv pdf file to download

--- app/sections/Skills.tsx
+++ app/sections/Skills.tsx
@@ -131,7 +131,7 @@ export const Skills = () => {
         </GridItem>
         <GridItem className="col-[none] sm:col-span-4" />
         <GridItem className="col-span-10 sm:col-span-6 flex flex-col sm:flex-row gap-16 items-center justify-around">
-          <a href="#" target="_blank">
+          <a href="/acun_guersoy_cv_2025.pdf" target="_blank">
             <div className="flex flex-col items-center justify-end h-40 min-w-[180px] hover:font-bold hover:text-blue-500 hover:cursor-pointer gap-2 border-b-2 border-transparent hover:border-blue-500 hover:rotate-2 hover:scale-[121%] transition-all">
               <DocumentIcon className="size-24 sm:size-32" />
               <p>PDF herunterladen</p>
