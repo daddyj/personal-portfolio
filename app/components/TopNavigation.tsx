cf07d7c3eb78663026a554412e377fc849f1aa57
Acun Guersoy <acun.guersoy@googlemail.com>
2025-03-02 19:10:56 +0100

    refactor: create reusable navigation item for desktop and mobile

--- app/components/TopNavigation.tsx
+++ app/components/TopNavigation.tsx
@@ -4,9 +4,9 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 
-import { sections } from '@/app/lib/types'
+import { HomeSection, sections } from '@/app/lib/types'
 import { useNavigationContext } from '@/app/lib/useNavigationContext'
 
 const scrollToSection = (sectionId: string) => {
@@ -18,26 +18,31 @@ const scrollToSection = (sectionId: string) => {
 }
 
 export const TopNavigation = () => {
-  const { currentSection, fullyVisible } = useNavigationContext()
-  const [isNextSectionAvailable, setIsNextSectionAvailable] = useState(true)
+  const { currentSection, fullyVisible, scrollDirection } =
+    useNavigationContext()
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
-  const handleArrowClick = useCallback(() => {
-    console.log('arrow click action with current section', currentSection)
+  const handleArrowDownClick = useCallback(() => {
     const currentIndex = sections.findIndex(
       (section) => currentSection === section
     )
     const nextIndex =
       fullyVisible === currentSection ? currentIndex + 1 : currentIndex
     scrollToSection(sections[nextIndex])
-    if (nextIndex === sections.length - 1) {
-      setIsNextSectionAvailable(false)
-    }
   }, [currentSection, fullyVisible])
 
-  useEffect(() => {
-    console.log({ currentSection })
-  }, [currentSection])
+  const handleArrowUpClick = () => {
+    scrollToSection('hero')
+  }
+
+  const handleTopNavigationClick = (section?: HomeSection) => () => {
+    if (section) scrollToSection(section)
+  }
+
+  const handleTopNavigationMobileClick = (section?: HomeSection) => () => {
+    if (section) scrollToSection(section)
+    setIsMobileMenuOpen(false)
+  }
 
   return (
     <nav className="fixed z-10 flex w-screen items-center gap-4 bg-[var(--background)] p-4 px-8 text-sm sm:gap-8 sm:px-16 sm:text-2xl">
@@ -66,110 +71,125 @@ export const TopNavigation = () => {
       </div>
 
       <div className="hidden flex-1 justify-end gap-4 sm:flex">
-        <div
-          onClick={() => {
-            scrollToSection('projects')
-          }}
-          className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'projects' ? 'border-b-blue-500 text-blue-500' : ''}`}
-        >
-          projekte
-        </div>
-        <div
-          onClick={() => {
-            scrollToSection('about')
-          }}
-          className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'about' ? 'border-b-blue-500 text-blue-500' : ''}`}
-        >
-          über mich
-        </div>
-        <div
-          onClick={() => {
-            scrollToSection('skillsTech')
-          }}
-          className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection && ['skillsTech', 'skillsSocial', 'skillsCv'].includes(currentSection) ? 'border-b-blue-500 text-blue-500' : ''}`}
-        >
-          kompetenzen
-        </div>
-        <div
-          onClick={() => {
-            scrollToSection('contact')
-          }}
-          className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'contact' ? 'border-b-blue-500 text-blue-500' : ''}`}
-        >
-          kontakt
-        </div>
+        <TopNavigationItem
+          isSelected={currentSection === 'projects'}
+          label="projekte"
+          onClick={handleTopNavigationClick}
+          section="projects"
+        />
+        <TopNavigationItem
+          isSelected={currentSection === 'about'}
+          label="über mich"
+          onClick={handleTopNavigationClick}
+          section="about"
+        />
+        <TopNavigationItem
+          isSelected={
+            currentSection &&
+            ['skillsTech', 'skillsSocial', 'skillsCv'].includes(currentSection)
+          }
+          label="kompetenzen"
+          onClick={handleTopNavigationClick}
+          section="skillsTech"
+        />
+        <TopNavigationItem
+          isSelected={currentSection === 'contact'}
+          label="kontakt"
+          onClick={handleTopNavigationClick}
+          section="contact"
+        />
       </div>
 
-      {isNextSectionAvailable && (
+      <TopNavigationMobileSheet
+        open={isMobileMenuOpen}
+        currentSection={currentSection}
+        onClick={handleTopNavigationMobileClick}
+      />
+
+      {scrollDirection === 'down' && (
         <div
-          className="animate animate-fade fixed bottom-16 left-16 hidden sm:block"
-          onClick={handleArrowClick}
+          className="animate animate-fade fixed bottom-4 left-4 sm:bottom-16 sm:left-16"
+          onClick={handleArrowDownClick}
         >
           <ArrowDownIcon className="hover:animate-infinite animate-once size-32 animate-pulse hover:cursor-pointer hover:text-blue-500" />
         </div>
       )}
-      {!isNextSectionAvailable && (
+      {scrollDirection === 'up' && (
         <div
-          className="animate animate-fade fixed bottom-16 left-16 hidden sm:block"
-          onClick={() => {
-            scrollToSection('hero')
-            setIsNextSectionAvailable(true)
-            // setTimeout(() => { setIsChangedByScroll(false) }, 1000)
-          }}
+          className="animate animate-fade fixed bottom-4 left-4 sm:bottom-16 sm:left-16"
+          onClick={handleArrowUpClick}
         >
           <ArrowUpIcon className="hover:animate-infinite animate-once size-32 animate-pulse hover:cursor-pointer hover:text-blue-500" />
         </div>
       )}
-      {isMobileMenuOpen && (
-        <div className="animate-fade animate-duration-420 fixed top-0 left-0 flex h-screen w-screen flex-col items-center gap-4 bg-[var(--background)] p-8 text-4xl">
-          <div
-            className="flex w-full justify-end"
-            onClick={() => {
-              setIsMobileMenuOpen(false)
-            }}
-          >
-            <XMarkIcon className="size-24" />
-          </div>
-          <div className="flex flex-1 flex-col justify-center">
-            <div
-              onClick={() => {
-                scrollToSection('projects')
-                setIsMobileMenuOpen(false)
-              }}
-              className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'projects' ? 'border-b-blue-500 text-blue-500' : ''}`}
-            >
-              projekte
-            </div>
-            <div
-              onClick={() => {
-                scrollToSection('about')
-                setIsMobileMenuOpen(false)
-              }}
-              className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'about' ? 'border-b-blue-500 text-blue-500' : ''}`}
-            >
-              über mich
-            </div>
-            <div
-              onClick={() => {
-                scrollToSection('skillsTech')
-                setIsMobileMenuOpen(false)
-              }}
-              className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection && ['skillsTech', 'skillsSocial', 'skillsCv'].includes(currentSection) ? 'border-b-blue-500 text-blue-500' : ''}`}
-            >
-              kompetenzen
-            </div>
-            <div
-              onClick={() => {
-                scrollToSection('contact')
-                setIsMobileMenuOpen(false)
-              }}
-              className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${currentSection === 'contact' ? 'border-b-blue-500 text-blue-500' : ''}`}
-            >
-              kontakt
-            </div>
-          </div>
-        </div>
-      )}
     </nav>
+  )
+}
+
+interface NavigationItemProps {
+  isSelected: boolean
+  onClick: (toSection?: HomeSection) => () => void
+  label: string
+  section: HomeSection
+}
+
+const TopNavigationItem = ({
+  isSelected,
+  onClick,
+  label,
+  section,
+}: NavigationItemProps) => {
+  return (
+    <div
+      onClick={onClick(section)}
+      className={`border-b-2 border-transparent transition-all hover:cursor-pointer hover:border-b-blue-500 hover:text-blue-500 ${isSelected ? 'border-b-blue-500 text-blue-500' : ''}`}
+    >
+      {label}
+    </div>
+  )
+}
+
+const TopNavigationMobileSheet = ({
+  open,
+  currentSection,
+  onClick,
+}: Pick<NavigationItemProps, 'onClick'> & {
+  currentSection: HomeSection
+  open: boolean
+}) => {
+  if (!open) return null
+
+  return (
+    <div className="animate-fade animate-duration-420 fixed top-0 left-0 flex h-screen w-screen flex-col items-center gap-4 bg-[var(--background)] p-8 text-4xl">
+      <div className="flex w-full justify-end" onClick={onClick()}>
+        <XMarkIcon className="size-24" />
+      </div>
+      <div className="flex flex-1 flex-col justify-center">
+        <TopNavigationItem
+          isSelected={currentSection === 'projects'}
+          label="projekte"
+          onClick={onClick}
+          section="projects"
+        />
+        <TopNavigationItem
+          isSelected={currentSection === 'about'}
+          label="über mich"
+          onClick={onClick}
+          section="about"
+        />
+        <TopNavigationItem
+          isSelected={currentSection === 'skillsTech'}
+          label="kompetenzen"
+          onClick={onClick}
+          section="skillsTech"
+        />
+        <TopNavigationItem
+          isSelected={currentSection === 'contact'}
+          label="kontakt"
+          onClick={onClick}
+          section="contact"
+        />
+      </div>
+    </div>
   )
 }
