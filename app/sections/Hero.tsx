812f547ee9d20f9d0bfd8a681af6160995261d32
Acun Guersoy <acun.guersoy@googlemail.com>
2025-05-21 18:04:26 +0200

    feat: add picture of me in hero section
    and make it mobile ready

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { useEffect, useRef } from 'react'
 
 import { Grid, GridItem } from '@/app/components/Grid'
@@ -19,30 +18,36 @@ export const Hero = () => {
     <Grid
       ref={gridWrapper}
       id="hero"
-      rows={`[auto_auto_auto_auto]`}
-      className="h-screen"
+      className="rows-[auto_auto_auto] h-screen"
     >
-      <GridItem className="col-span-10" />
+      <GridItem className="col-span-10 flex items-center justify-center sm:justify-start">
+        <div
+          className="h-40 w-40 rounded-full bg-blue-500 bg-center bg-no-repeat sm:h-56 sm:w-56"
+          style={{
+            backgroundImage: 'url(/skills-cv-section-me.jpeg)',
+            backgroundSize: '100%',
+          }}
+        />
+      </GridItem>
 
-      <GridItem className="animate-fade-down animate-once animate-duration-1200 animate-ease-out animate-delay-440 col-span-8 flex flex-col">
+      <GridItem className="animate-fade-down animate-once animate-duration-1200 animate-ease-out animate-delay-440 col-span-10 flex flex-col sm:col-span-8">
         <h1 className="text-4xl uppercase sm:text-6xl">Acun Gürsoy</h1>
         <h1 className="text-4xl uppercase sm:text-6xl">
           Freelance Web & Mobile Entwickler
         </h1>
         <h2 className="text-xl uppercase">React / React-Native</h2>
       </GridItem>
-      <GridItem className="col-span-2" />
+      <GridItem className="col-span-0 sm:col-span-2" />
 
-      <GridItem className="col-span-10" />
-      <div className="absolute right-16 bottom-16">
-        <Image
-          src="/certified.png"
-          alt="Certified"
-          width={256}
-          height={256}
-          className="text-white-500 invert dark:invert-0"
+      <GridItem className="col-span-10 flex items-end justify-end">
+        <div
+          className="h-40 w-40 rounded-full bg-center bg-no-repeat sm:my-0 sm:h-64 sm:w-64"
+          style={{
+            backgroundImage: 'url(/certified.png)',
+            backgroundSize: '100%',
+          }}
         />
-      </div>
+      </GridItem>
     </Grid>
   )
 }
