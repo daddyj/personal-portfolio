605285c193275f073acf75fce6925b3e63d985de
Acun Guersoy <acun.guersoy@googlemail.com>
2025-09-09 19:07:30 +0200

    feat: introduce new project presentation design layout

---
+++ app/sections/Projects/Project.tsx
@@ -0,0 +1,112 @@
+import 'atropos/css'
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/pagination'
+
+import Image from 'next/image'
+import { JSX, useRef } from 'react'
+import { Autoplay, EffectCards, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+export const Project = ({
+  title,
+  customer,
+  description,
+  technologies,
+  images,
+  projectId,
+}: {
+  title: string
+  customer: string
+  description: string
+  technologies: JSX.Element[]
+  images: string[]
+  projectId: string
+}) => {
+  const progressCircle = useRef<SVGSVGElement>(null)
+  const progressContent = useRef<HTMLSpanElement>(null)
+  // const onAutoplayTimeLeft = (
+  //   s: SwiperClass,
+  //   time: number,
+  //   progress: number
+  // ) => {
+  //   progressCircle.current?.style.setProperty(
+  //     '--progress',
+  //     (1 - progress).toString()
+  //   )
+  //   progressContent.current?.textContent = `${Math.ceil(time / 1000)}s`
+  // }
+
+  return (
+    <div
+      className={`atropos project-${projectId}`}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <div className="atropos-scale">
+        <div className="atropos-rotate">
+          <div className="atropos-inner">
+            <div className="flex h-full gap-16 rounded-2xl bg-blue-500/10 px-16 py-8">
+              <div className="flex h-full flex-1 flex-col gap-4">
+                <h3 className="text-6xl font-thin">{title}</h3>
+                <h4 className="text-2xl font-normal">Kunde: {customer}</h4>
+                <p className="flex-1 text-2xl">Beschreibung: {description}</p>
+                <div>
+                  <Swiper
+                    effect="cards"
+                    grabCursor
+                    modules={[EffectCards]}
+                    className="swiperSkillsCvTechnologies"
+                    onDrag={(e) => {
+                      e.stopPropagation()
+                    }}
+                  >
+                    {technologies.map((technology) => (
+                      <SwiperSlide key={technology.key}>
+                        {technology}
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+              </div>
+              <div className="flex h-full w-1/2 flex-1 flex-col justify-center">
+                <Swiper
+                  cssMode
+                  spaceBetween={30}
+                  centeredSlides={false}
+                  autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: true,
+                  }}
+                  pagination
+                  modules={[Autoplay, Pagination]}
+                  // onAutoplayTimeLeft={onAutoplayTimeLeft}
+                  className="swiperProject"
+                >
+                  {images.map((imageUrl) => (
+                    <SwiperSlide key={imageUrl}>
+                      <div className="h-11/12 w-full">
+                        <Image
+                          src={imageUrl}
+                          alt={imageUrl}
+                          width={1920}
+                          height={1080}
+                          className="h-full w-full"
+                        />
+                      </div>
+                    </SwiperSlide>
+                  ))}
+                  <div className="autoplay-progress" slot="container-end">
+                    <svg viewBox="0 0 48 48" ref={progressCircle}>
+                      <circle cx="24" cy="24" r="20"></circle>
+                    </svg>
+                    <span ref={progressContent}></span>
+                  </div>
+                </Swiper>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/sections/Projects/Projects.tsx
+++ app/sections/Projects/Projects.tsx
@@ -1,103 +1,54 @@
-import { useEffect, useRef, useState } from 'react'
+import 'atropos/css'
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import './styles.css'
+
+import Atropos, { AtroposInstance } from 'atropos'
+import { useEffect, useRef } from 'react'
+import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { Grid, GridItem } from '@/app/components/Grid'
 import {
   Firebase,
-  GCloud,
+  FirebaseSvg,
   Nextjs,
   React,
-  ReactNative,
+  ReactNativeSvg,
   Tamagui,
 } from '@/app/components/Icons'
+import { JavascriptSvg } from '@/app/components/Icons/Javascript'
 import { MaterialUi } from '@/app/components/Icons/MaterialUi'
 import { useNavigationContext } from '@/app/lib/useNavigationContext'
 import { useViewportIntersect } from '@/app/lib/useViewportIntersect'
 
-import { ItemPreview } from './ItemPreview'
-import { NavItem } from './NavItem'
-
-const projects = ['myla', 'theanswer-hp', 'myla-portal'] as const
-export type ProjectListItem = (typeof projects)[number]
-
-const projectDetails = {
-  myla: {
-    navLabel: 'Myla',
-    role: 'Fullstack Entwicklung App',
-    techStack: [
-      <ReactNative key="react-native" />,
-      <Tamagui key="tamagui" />,
-      <Firebase key="firebase" />,
-      <GCloud key="google-cloud" />,
-    ],
-    images: ['/myla-3.png', '/myla-2.png', '/myla-1.png'],
-    description: (
-      <>
-        Die App Myla hat als Ziel eine individuelle Lernbegleitung für jedes
-        Kind anzubieten. Es nutzt künstliche Intelligenz in einer geführten und
-        interaktiven Erfahrung für die Kinder, so dass Sie spielerisch das
-        selbstständige Lernen erhalten. <br />
-        Die App habe ich vollständig entwickelt und im iOS AppStore für iPads
-        und iPhones veröffentlicht. Android ist auch in Planung. Mehr
-        Informationen findest Du{' '}
-        <a
-          href="https://the-answer.org"
-          target="_blank"
-          className="border-b-2 hover:text-blue-500"
-        >
-          hier
-        </a>
-      </>
-    ),
-  },
-  'theanswer-hp': {
-    navLabel: 'THE ANSWER GmbH Homepage',
-    role: 'Fullstack Entwicklung Homepage und UI Design',
-    techStack: [
-      <React key="react" />,
-      <Nextjs key="nextjs" />,
-      <MaterialUi key="material-ui" />,
-    ],
-    images: ['/the-answer-3.png', '/the-answer-2.png', '/the-answer-1.png'],
-    description: (
-      <>
-        Im Rahmen meiner Firmengründung mit zwei Freunden, um die App Myla auch
-        offiziell zu vertreiben, habe ich die Firmenseite entwickelt. Ich habe
-        mich bewusst dazu entschieden es ohne Baukasten oder CRM zu machen um
-        hier hauptsächlich ein Referenzprojekt als Grundlage zur Diskussion für
-        Kombinationen von Technologien zu haben. <br />
-        Ich finde es immer noch spannend und offen zu sagen, welche Kombination
-        &quot;die Beste&quot; ist.
-      </>
-    ),
-  },
-  'myla-portal': {
-    navLabel: 'Myla Schulportal',
-    role: 'Fullstack Entwicklung Portal und UI Design',
-    techStack: [
-      <React key="react" />,
-      <Nextjs key="nextjs" />,
-      <MaterialUi key="material-ui" />,
-      <Firebase key="firebase" />,
-    ],
-    images: ['/myla-portal-3.png', '/myla-portal-2.png', '/myla-portal-1.png'],
-    description: (
-      <>
-        Die App Myla wird sowohl auf dem Privatmarkt vertrieben, als auch für
-        Schulen auf geschäftlicher Ebene über eine jährliche Schullizenz pro
-        Gerät. Hierfür habe ich eine minimale Form von Portal entwickelt um zum
-        Einen den time-to-market nicht zu verzögern und zum Anderen dem Benutzer
-        nur die Informationsmenge und Funktionsvielfalt mitzugeben die zu
-        jeweiligem Entwickklungsstand am hilfreichsten ist.
-      </>
-    ),
-  },
-}
+import { Project } from './Project'
 
 export const Projects = () => {
   const { setCurrentSection, setFullyVisible } = useNavigationContext()
   const gridWrapper = useRef<HTMLDivElement>(null)
   const { isVisible, isFullyVisible } = useViewportIntersect(gridWrapper)
-  const [currentProject, setCurrentProject] = useState<ProjectListItem>('myla')
+
+  const myAtropos = useRef<AtroposInstance | null>(null)
+
+  useEffect(() => {
+    myAtropos.current = Atropos({
+      el: '.project-1',
+      activeOffset: 20,
+      shadowScale: 1.05,
+    })
+    Atropos({
+      el: '.project-2',
+      activeOffset: 20,
+      shadowScale: 1.05,
+    })
+    Atropos({
+      el: '.project-3',
+      activeOffset: 20,
+      shadowScale: 1.05,
+    })
+  })
 
   useEffect(() => {
     if (isVisible) setCurrentSection('projects')
@@ -105,28 +56,68 @@ export const Projects = () => {
   }, [isFullyVisible, isVisible, setCurrentSection, setFullyVisible])
 
   return (
-    <Grid
-      ref={gridWrapper}
-      id="projects"
-      className="grid-rows-[auto] lg:grid-rows-[auto_80px_auto_120px_1fr]"
-    >
+    <Grid ref={gridWrapper} id="projects" className="grid-rows-[auto_1fr]">
       <GridItem className="col-span-10">
-        <h1 className="text-4xl lg:text-6xl">Meine aktiven Projekte</h1>
+        <h1 className="text-4xl lg:text-6xl">Beispiele meiner Arbeit</h1>
       </GridItem>
 
-      <GridItem className="col-span-10" />
-
-      <GridItem className="text-md col-span-10 flex flex-col gap-4 pb-8 lg:col-span-3 lg:gap-8 lg:pb-0 lg:text-4xl">
-        {projects.map((projectKey: ProjectListItem) => (
-          <NavItem
-            key={`nav_${projectKey}`}
-            onClick={() => setCurrentProject(projectKey)}
-            selected={currentProject === projectKey}
-            label={projectDetails[projectKey].navLabel}
-          />
-        ))}
+      <GridItem className="col-span-10">
+        <Swiper
+          effect={'coverflow'}
+          centeredSlides={true}
+          navigation
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiperProjects"
+        >
+          <SwiperSlide>
+            <Project
+              projectId="1"
+              title="White Label CMS Lösung"
+              customer="tinkerbrain GmbH"
+              description="TBD"
+              technologies={[<JavascriptSvg key="javascript" />]}
+              images={['/apostrophe-1.jpg', '/apostrophe-2.jpg']}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project
+              projectId="2"
+              title="Myla"
+              customer="The Answer GmbH"
+              description="Die App Myla hat als Ziel eine individuelle Lernbegleitung für jedes Kind anzubieten. Es nutzt künstliche Intelligenz in einer geführten und interaktiven Erfahrung für die Kinder, so dass Sie spielerisch das selbstständige Lernen erhalten. Die App habe ich vollständig entwickelt und im iOS AppStore für iPads und iPhones veröffentlicht. Android ist auch in Planung. Mehr Informationen findest Du hier"
+              technologies={[
+                <ReactNativeSvg key="react-native" />,
+                <FirebaseSvg key="firebase" />,
+                <Tamagui key="tamagui" />,
+              ]}
+              images={['/myla-3.png', '/myla-2.png', '/myla-1.png']}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project
+              projectId="3"
+              title="Queer*link"
+              customer="Queer*link e.V."
+              description="Die Website Queer*link e.V. ist eine Plattform für Queer* Menschen und ihre Angehörigen. Sie bietet Informationen, Ressourcen und eine Community für alle, die sich in der LGBTQ+ Community engagieren. Ich habe die Website vollständig entwickelt und in Eigenleistung betreut. Sie dient als Basis für die Entwicklung der App Queer*link welches als nächstes in Planung ist."
+              technologies={[
+                <React key="react" />,
+                <Nextjs key="nextjs" />,
+                <MaterialUi key="material-ui" />,
+                <Firebase key="firebase" />,
+              ]}
+              images={['/queerlink-1.jpg', '/queerlink-2.jpg']}
+            />
+          </SwiperSlide>
+        </Swiper>
       </GridItem>
-      <ItemPreview {...projectDetails[currentProject]} />
     </Grid>
   )
 }
