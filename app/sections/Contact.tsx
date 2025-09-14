7c20ff3a63698a8c5128464082cd0e2700f3206b
Acun Guersoy <acun.guersoy@googlemail.com>
2025-09-14 15:08:21 +0200

    feat: add latest cv (DE) to the page

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,4 +1,5 @@
 import {
+  DocumentArrowDownIcon,
   EnvelopeIcon,
   EnvelopeOpenIcon,
   PhoneIcon,
@@ -55,27 +56,36 @@ export const Contact = () => {
             href="mailto:acun.guersoy@gmail.com"
             className="group text-md lg:hover:text-7x flex items-center gap-4 uppercase transition-all duration-210 hover:bg-black hover:text-blue-500 lg:text-6xl"
           >
-            <div className="hidden transition-opacity duration-210 group-hover:block">
+            <div className="block transition-opacity duration-210 group-hover:hidden">
               <EnvelopeIcon className="size-12 lg:size-24" />
             </div>
-            <div className="block group-hover:hidden">
+            <div className="hidden group-hover:block">
               <EnvelopeOpenIcon className="size-12 lg:size-24" />
             </div>
-            <p className="sm:font-thin">E-Mail</p>
+            <p className="group-hover:font-bold sm:font-thin">E-Mail</p>
           </a>
           <a
             href="tel:+4915258517763"
             className="group text-md lg:hover:text-7x flex items-center gap-4 uppercase transition-all duration-210 hover:bg-black hover:text-blue-500 lg:text-6xl"
           >
             <PhoneIcon className="size-12 rotate-136 transition-transform duration-210 group-hover:rotate-24 lg:size-24" />
-            <p className="sm:font-thin">Telefon</p>
+            <p className="group-hover:font-bold sm:font-thin">Telefon</p>
           </a>
           <a
             href="https://www.linkedin.com/in/acun-g%C3%BCrsoy-83b8ab139/"
+            target="_blank"
             className="group text-md lg:hover:text-7x flex items-center gap-4 uppercase transition-all duration-210 hover:bg-black hover:text-blue-500 lg:text-6xl"
           >
             <GlobeIcon className="size-12 transition-transform duration-210 group-hover:rotate-180 lg:size-24" />
-            <p className="sm:font-thin">LinkedIn</p>
+            <p className="group-hover:font-bold sm:font-thin">LinkedIn</p>
+          </a>
+          <a
+            href="/docs/acun_guersoy_cv_25.pdf"
+            target="_blank"
+            className="group text-md lg:hover:text-7x flex items-center gap-4 uppercase transition-all duration-210 hover:bg-black hover:text-blue-500 lg:text-6xl"
+          >
+            <DocumentArrowDownIcon className="size-12 transition-transform duration-210 group-hover:scale-125 lg:size-24" />
+            <p className="group-hover:font-bold sm:font-thin">Lebenslauf</p>
           </a>
         </GridItem>
         <GridItem className="animate-fade-down animate-once animate-duration-1200 animate-ease-out animate-delay-640 col-span-10 flex items-end justify-center text-sm lg:items-start lg:text-2xl">
