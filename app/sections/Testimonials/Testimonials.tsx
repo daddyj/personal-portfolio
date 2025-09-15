47cc97345af118d4f87db7515e05b87fac862e81
Acun Guersoy <acun.guersoy@googlemail.com>
2025-09-15 09:36:39 +0200

    fix: make testimonials look good on mobile devices

--- app/sections/Testimonials/Testimonials.tsx
+++ app/sections/Testimonials/Testimonials.tsx
@@ -8,41 +8,11 @@ import { useEffect, useRef } from 'react'
 import { Autoplay, Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-import { Grid, GridItem } from '../components/Grid'
-import { PixelGlitchScreen } from '../components/PixelGlitchScreen'
-import { useNavigationContext } from '../lib/useNavigationContext'
-import { useViewportIntersect } from '../lib/useViewportIntersect'
-
-const testimonials = [
-  {
-    name: 'Vera Herberholt',
-    role: 'Assistenz der Geschäftsführung',
-    company: 'tinkerbrain',
-    quote:
-      'Acun hat unser Projekt technisch komplett umgesetzt, zuverlässig betreut und war stets ein flexibler, vertrauensvoller Ansprechpartner.',
-  },
-  {
-    name: 'Mathias Eisenhut',
-    role: 'Projektleiter',
-    company: 'Queer*link',
-    quote:
-      'Top-Entwickler: Acun Gürsoy übertraf mit kreativen Ideen für unsere LGBTQ+ Webseite alle Erwartungen. Stets lösungsorientiert & absolut empfehlenswert',
-  },
-  {
-    name: 'Tyll Selhorst',
-    role: 'Mitgründer',
-    company: 'The Answer GmbH',
-    quote:
-      'Acun hat das technische Feld vollständig betreut und uns zusätzlich bei Projektmanagement und agiler Arbeitsweise weiter gefördert!',
-  },
-  {
-    name: 'Sebastian Weber',
-    role: 'Agile Coach',
-    company: 'DeepUp GmbH',
-    quote:
-      'Acun ist ein Vorbild für Servant Leadership und ganzheitliche Organisationsentwicklung. Er war Inspiration und Sparringspartner für mich.',
-  },
-]
+import { Grid, GridItem } from '../../components/Grid'
+import { PixelGlitchScreen } from '../../components/PixelGlitchScreen'
+import { useNavigationContext } from '../../lib/useNavigationContext'
+import { useViewportIntersect } from '../../lib/useViewportIntersect'
+import { testimonials } from './constants'
 
 export const Testimonials = () => {
   const { setCurrentSection, setFullyVisible } = useNavigationContext()
@@ -81,21 +51,21 @@ export const Testimonials = () => {
             modules={[Autoplay, Pagination, Navigation]}
             spaceBetween={30}
             slidesPerView={1}
-            pagination={{ clickable: true }}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
+            pagination
+            // autoplay={{
+            //   delay: 5000,
+            //   disableOnInteraction: false,
+            // }}
             className="h-[400px] w-full"
             navigation
           >
             {testimonials.map((testimonial, index) => (
               <SwiperSlide key={index}>
-                <div className="flex h-full flex-col gap-6 rounded-lg p-8 px-40">
-                  <blockquote className="text-center text-2xl text-gray-200 italic md:text-4xl">
+                <div className="flex h-full flex-col gap-16 rounded-lg p-4 px-8 sm:justify-between sm:p-8">
+                  <blockquote className="text-center text-xl text-gray-200 italic sm:text-4xl">
                     &quot;{testimonial.quote}&quot;
                   </blockquote>
-                  <div className="mt-auto">
+                  <div className="flex flex-col items-center text-center sm:mt-auto sm:items-start">
                     <p className="text-lg font-medium text-white">
                       {testimonial.name}
                     </p>
---
+++ app/sections/Testimonials/constants.ts
@@ -0,0 +1,30 @@
+export const testimonials = [
+  {
+    name: 'Vera Herberholt',
+    role: 'Prokuristin',
+    company: 'tinkerbrain',
+    quote:
+      'Acun hat unser Projekt technisch komplett umgesetzt, zuverlässig betreut und war stets ein flexibler, vertrauensvoller Ansprechpartner.',
+  },
+  {
+    name: 'Mathias Eisenhut',
+    role: 'Projektleiter',
+    company: 'Queer*link',
+    quote:
+      'Top-Entwickler: Acun Gürsoy übertraf mit kreativen Ideen für unsere LGBTQ+ Webseite alle Erwartungen. Stets lösungsorientiert & absolut empfehlenswert',
+  },
+  {
+    name: 'Tyll Selhorst',
+    role: 'Mitgründer',
+    company: 'The Answer GmbH',
+    quote:
+      'Acun hat das technische Feld vollständig betreut und uns zusätzlich bei Projektmanagement und agiler Arbeitsweise weiter gefördert!',
+  },
+  {
+    name: 'Sebastian Weber',
+    role: 'Agile Coach',
+    company: 'DeepUp GmbH',
+    quote:
+      'Acun ist ein Vorbild für Servant Leadership und ganzheitliche Organisationsentwicklung. Er war Inspiration und Sparringspartner für mich.',
+  },
+]
